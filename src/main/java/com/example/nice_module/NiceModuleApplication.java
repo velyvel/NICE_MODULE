20197d7045336d6224aa5d263d34a8ff86fc734c
강수림 <eathergs012@gmail.com>
2025-04-06 09:58:37 +0900

    first_commit

---
+++ src/main/java/com/example/nice_module/NiceModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.nice_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceModuleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NiceModuleApplication.class, args);
+  }
+
+}
